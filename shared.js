07054adc83d038807416a41b1d15ec90c843a12d
Alec Flett <alecflett@chromium.org>
2012-05-07 14:32:04 -0700

    remove console lines

--- shared.js
+++ shared.js
@@ -6,14 +6,12 @@ function requestWebkitVersion(callback) {
   xhr.onreadystatechange = function() {
     console.log("onreadystatechange: ", xhr);
     if (xhr.readyState == 4) {
-      console.log("Ready state: ", xhr);
       var new_lines = [];
       var lines = xhr.responseText.split('\n');
       for (var i = 0; i < lines.length; i++) {
         var line = lines[i];
         var revision = line.indexOf('"webkit_revision":');
         if (revision != -1) {
-          console.log("Found webkit_version line: ", line);
           var val = line.split(":")[1].split('"')[1];
           if (callback) {
             callback(val);
